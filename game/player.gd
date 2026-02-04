825f5f3df1c9ed0f59bbb27c515f2362ea900014
Tristan Itoh <itohtristan@gmail.com>
2026-02-03 22:04:12 -0700

    Created NPCs, updated map

--- game/player.gd
+++ game/player.gd
@@ -1,11 +1,15 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
-
+const SPEED = 70.0
+const ACCEL = 100.0
+const FRICTION = 50.0
 
 func _physics_process(delta):
 	var direction = Input.get_vector("left", "right", "up", "down")
-	velocity = direction * SPEED
+
+	if direction != Vector2.ZERO:
+		velocity = velocity.move_toward(direction * SPEED, ACCEL * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+
 	move_and_slide()
-	
-	
