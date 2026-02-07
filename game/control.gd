a6c9d9e8d13986ec27118a4f3200ca1f0498e10c
NoahKYi <yinoah8@gmail.com>
2026-02-06 21:38:04 -0700

    My Change (Dw it make sense)

--- game/control.gd
+++ game/control.gd
@@ -8,16 +8,6 @@ var min = 0
 var defsec = 0 # Default Seconds
 var defmin = 5 # Default Minutes
 
-
-func _on_timeout():
-	if sec ==0:
-		if min > 0:
-			min -= 1
-			sec = 60		#Pass the Timer
-	sec-=1
-
-	$Label.text = str(sec)+": "+str(min)
-	
 func Reset_Timer():
 	sec = defsec
 	min = defmin
