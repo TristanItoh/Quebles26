ab965db87ded7bae49130b79aea161319daed3a8
NoahKYi <yinoah8@gmail.com>
2026-02-06 22:42:46 -0700

    Added the label!
    Change the location in the future ;<)

--- game/control.gd
+++ game/control.gd
@@ -1,30 +1,43 @@
 extends Control
 
+
 func _ready():
 	Reset_Timer()
+	_on_timeout()
+	
 
 var sec = 0
 var min = 0
 var defsec = 0 # Default Seconds
 var defmin = 5 # Default Minutes
+var parent = get_parent()
 
 func Reset_Timer():
 	sec = defsec
 	min = defmin
 	
+func center_label(delta):
+	await get_tree().process_frame  
+	var player = $Player
+	var label = $Label               
+	label.pivot_offset = label.size / 2
+	label.global_position = player.position + Vector2(0, -60)
+
+	  # 60px above the player
+
 func _on_timeout():
-	if min == 0 and sec == 0:
+	if min == 0 and sec == 0: # Game Over
 		print("Game Over")
 		$Timer.stop()
 		return
 		
-	if sec == 0:
+	if sec == 0: # Minute Decreases
 		min -= 1
 		sec = 59
-		return
-		
-	else:
+		upd_script()
+	else: # Seconds Decrease
 		sec -= 1
-		return
+		upd_script()
 		
+func upd_script():
 	$Label.text = "%02d:%02d" % [min, sec]
