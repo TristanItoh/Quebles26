d4b11b3b9328d2046df85c40f2378c3b0c8df274
Tristan Itoh <itohtristan@gmail.com>
2026-02-19 22:57:45 -0700

    yea

--- game/scripts/DialogueManager.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-var dialogue_ui = null
-
-func show(text, portrait, name):
-	if dialogue_ui:
-		await dialogue_ui.show_dialogue(text, portrait, name)
-	else:
-		push_error("DialogueUI not registered yet!")
